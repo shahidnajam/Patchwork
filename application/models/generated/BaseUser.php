73659eaa767363000a20f929c001ee28ba756d55
Daniel Pozzi <dpozzi@m2soft.com>
2010-07-20 05:38:33 +0200

    Doctrine user model

--- application/models/generated/BaseUser.php
+++ application/models/generated/BaseUser.php
@@ -7,22 +7,20 @@
  * 
  * @property integer $id
  * @property enum $gender
+ * @property string $username
  * @property string $firstname
  * @property string $lastname
  * @property string $email
- * @property string $mobile_number
- * @property string $preferred_language
+ * @property string $language
  * @property string $password
  * @property string $salt
  * @property enum $status
  * @property enum $role
- * @property Doctrine_Collection $ActivationCodes
- * @property Doctrine_Collection $LostPassword
  * 
  * @package    ##PACKAGE##
  * @subpackage ##SUBPACKAGE##
  * @author     ##NAME## <##EMAIL##>
- * @version    SVN: $Id: Builder.php 6820 2009-11-30 17:27:49Z jwage $
+ * @version    SVN: $Id: Builder.php 7490 2010-03-29 19:53:27Z jwage $
  */
 abstract class BaseUser extends Doctrine_Record
 {
@@ -44,6 +42,10 @@ abstract class BaseUser extends Doctrine_Record
               2 => 'nn',
              ),
              ));
+        $this->hasColumn('username', 'string', 255, array(
+             'type' => 'string',
+             'length' => '255',
+             ));
         $this->hasColumn('firstname', 'string', 255, array(
              'type' => 'string',
              'length' => '255',
@@ -54,13 +56,10 @@ abstract class BaseUser extends Doctrine_Record
              ));
         $this->hasColumn('email', 'string', 255, array(
              'type' => 'string',
+             'notnull' => true,
              'length' => '255',
              ));
-        $this->hasColumn('mobile_number', 'string', 255, array(
-             'type' => 'string',
-             'length' => '255',
-             ));
-        $this->hasColumn('preferred_language', 'string', 5, array(
+        $this->hasColumn('language', 'string', 5, array(
              'type' => 'string',
              'default' => 'en_GB',
              'length' => '5',
@@ -103,14 +102,6 @@ abstract class BaseUser extends Doctrine_Record
     public function setUp()
     {
         parent::setUp();
-        $this->hasMany('ActivationCode as ActivationCodes', array(
-             'local' => 'id',
-             'foreign' => 'user_id'));
-
-        $this->hasMany('LostPassword', array(
-             'local' => 'id',
-             'foreign' => 'user_id'));
-
         $timestampable0 = new Doctrine_Template_Timestampable();
         $softdelete0 = new Doctrine_Template_SoftDelete();
         $this->actAs($timestampable0);
