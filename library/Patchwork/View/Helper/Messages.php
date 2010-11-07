fad7254d58ab1e47e06f0b6d36e942ff95d5234d
Daniel Pozzi <bonndan76@googlemail.com>
2010-11-07 22:04:18 +0100

    Modified messages view helper and layout

--- library/Patchwork/View/Helper/Messages.php
+++ library/Patchwork/View/Helper/Messages.php
@@ -9,7 +9,7 @@
  */
 
 /**
- * View_Helper_Messages
+ * Patchwork_View_Helper_Messages
  *
  * Helper to render messages form Patchwork_Messenger or Zends
  * FlashMessenger. Renders divs with status css classed as passed to the
@@ -23,18 +23,26 @@
 class Patchwork_View_Helper_Messages extends Zend_View_Helper_Abstract
 {
     /**
-     * get messages from flash messenger (merged current and session)
-     * 
+     * blueprint statuses
+     */
+    const ERROR  = 'error';
+    const NOTICE = 'notice';
+    const ADDED  = 'added';
+    const SUCCES = 'success';
+    const REMOVED= 'removed';
+
+    /**
+     * get messages from flash messenger
      * @return array
      */
     private function getMessages()
     {
-        $messenger = Patchwork_Messenger::getFlashMessenger();
+        $messages = Zend_Controller_Action_HelperBroker::getStaticHelper(
+                'FlashMessenger')->getMessages();
+        $currentMessages = Zend_Controller_Action_HelperBroker::getStaticHelper(
+                'FlashMessenger')->getCurrentMessages();
 
-        return array_merge(
-            $messenger->getMessages(),
-            $messenger->getCurrentMessages()
-        );
+        return array_merge($messages, $currentMessages);
     }
 
     /**
@@ -43,7 +51,7 @@ class Patchwork_View_Helper_Messages extends Zend_View_Helper_Abstract
      * @param $translator An optional instance of Zend_Translate
      * @return string HTML of output messages
      */
-    public function Messages($returnArray = false)
+    public function flashMessages($translator = NULL)
     {
         $messages = $this->getMessages();
         if (count($messages) == 0) {
@@ -73,13 +81,35 @@ class Patchwork_View_Helper_Messages extends Zend_View_Helper_Abstract
         }
 
         foreach ($statMessages as $status => $messages) {
-            $output .= '<div class="' . $status . '">';
+            $output .= '<div class="' . $status . '"><ul>';
             foreach ($messages as $message) {
-                $output .= $message.' ';
+                $output .= '<li>' . $message . '</li>' . PHP_EOL;
             }
-            $output .= '</div>' . PHP_EOL;
+            $output .= '</ul></div>' . PHP_EOL;
         }
 
         return $output;
+    }
+
+    /**
+     * the fm
+     *
+     * @return Zend_Controller_Action_Helper_FlashMessenger
+     */
+    public static function getFlashMessenger()
+    {
+        return
+        Zend_Controller_Action_HelperBroker::getStaticHelper('FlashMessenger');
+    }
+
+    /**
+     * add an error to the flash messenger
+     *
+     * @param string $message error message
+     * @return void
+     */
+    public static function addError($message)
+    {
+        self::getFlashMessenger()->addMessage(array(self::ERROR => $message));
     }
 }
