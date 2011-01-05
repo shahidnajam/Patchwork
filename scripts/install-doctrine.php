771b7739b228ab254a6dcfc0555dd9e29310aab9
Daniel Pozzi <bonndan76@googlemail.com>
2011-01-06 00:10:18 +0100

    Working Doctrine installation script

--- scripts/install-doctrine.php
+++ scripts/install-doctrine.php
@@ -1,7 +1,12 @@
 <?php
+define('LIBRARY_PATH', dirname(__DIR__).'/library');
+
+require_once(LIBRARY_PATH . '/Installation/Dependency.php');
+require_once(LIBRARY_PATH . '/Installation/Dependency/Git.php');
+
 $doctrine = new Installation_Dependency_Git(
     'git://github.com/doctrine/doctrine1.git',
-    dirname(__DIR__).'/library',
+    LIBRARY_PATH,
     'lib/Doctrine*'
 );
 $doctrine->install();
