a02995de44a6f550bc9d81ca8a04f78eecefa7f1
Daniel Pozzi <bonndan76@googlemail.com>
2011-01-06 00:07:01 +0100

    Update doctrine install script

--- scripts/install-doctrine.php
+++ scripts/install-doctrine.php
@@ -2,6 +2,6 @@
 $doctrine = new Installation_Dependency_Git(
     'git://github.com/doctrine/doctrine1.git',
     dirname(__DIR__).'/library',
-    'Doctrine*'
+    'lib/Doctrine*'
 );
 $doctrine->install();
