30225c5d8608e4ea72f93ca8c3d8092d92ae37be
Daniel Pozzi <bonndan76@googlemail.com>
2011-02-22 21:29:00 +0100

    reconfigured bootstrap files to cli/zf tool usage

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -1,8 +1,45 @@
 <?php
+/**
+ * Patchwork
+ *
+ * @category    Application
+ * @package     Default
+ * @subpackage  Bootstrap
+ * @author      Daniel Pozzi <bonndan76@googlemail.com>
+ */
 
+/**
+ * Bootstrap class
+ *
+ * @package    Application
+ * @subpackage Default
+ * @author     Daniel Pozzi <bonndan76@googlemail.com>
+ */
 class Bootstrap extends Patchwork_Bootstrap
 {
+    /**
+     * init app-wide locale
+     *
+     * @return Zend_Locale
+     */
+    public function _initLocale()
+    {
+        $locale = new Zend_Locale();
+        Zend_Registry::set('Zend_Locale', $locale);
 
+        // Return it, so that it can be stored in the container
+        return $locale;
+    }
+
+    /**
+     * command line interface bootstrapping
+     *
+     *
+     */
+    public function _initCli()
+    {
+        $this->_initAppAutoload();
+    }
 
 }
 
